2fdc7189412a071d701669038686428c24a3225a
uysalGokhan <56603776+uysalGokhan@users.noreply.github.com>
2022-05-07 12:35:00 +0300

    Json Reader added

--- BlockSlidingGame/Assets/Scripts/InitPreFabs.cs
+++ BlockSlidingGame/Assets/Scripts/InitPreFabs.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class InitPreFabs : MonoBehaviour
 {
-    public GameObject Cube;
+    public GameObject CubePrefab;
+    public TextAsset textJSON;
+
+    [System.Serializable]
+    public class Cube
+    {
+        public int x;
+        public int y;
+        public int z;
+        public int x_scale;
+        public int y_scale;
+        public int z_scale;
+        public int x_direction;
+        public int y_direction;
+        public int z_direction;
+        public int score;
+    }
+
+    [System.Serializable]
+    public class CubeList
+    {
+        public Cube[] cube;
+    }
+
+    public CubeList cubeList = new CubeList();
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(Cube, new Vector3(-2, 0, 0), Quaternion.identity);
-        Instantiate(Cube, new Vector3(0, 0, 0), Quaternion.identity);
-        Instantiate(Cube, new Vector3(2, 0, 0), Quaternion.identity);
+        cubeList = JsonUtility.FromJson<CubeList>(textJSON.text);
+        for (int i = 0; i < cubeList.cube.Length; i++)
+        {
+            Cube c = cubeList.cube[i];
+            Instantiate(CubePrefab, new Vector3(c.x, c.y, c.z), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
---
+++ BlockSlidingGame/Assets/Scripts/JSONReader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONReader : MonoBehaviour
+{
+    public TextAsset textJSON;
+
+    [System.Serializable]
+
+    public class Cube
+    {
+        public float x;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
